d5b22522ef933d097587c5613a2195cba40d43b9
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-08-30 17:14:50 +0530

    For Loop | *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'Switch Case';
-  color = ''
+  title: string = 'Loop in Angular';
+  users = ['Messi', "Ronaldo", "Neymar", "Ramos", "DiMaria"]
+
+  usersDetails = [{
+    name: 'Messi',
+    email: 'messi@gmail.com',
+    phone:8888
+  },{
+    name: 'Ronaldo',
+    email: 'ronaldo@yahoo.com',
+    phone:11111
+  },{
+    name: 'Neymar',
+    email: 'neymar@hotmail.com',
+    phone:11111
+  },{
+    name: 'Ramos',
+    email: 'ramos@yahoo.com',
+    phone:11119
+  }]
 }
