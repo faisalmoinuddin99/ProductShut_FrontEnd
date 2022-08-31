71f68bdea08308132bf11e06a517844d4983e883
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-08-31 09:51:45 +0530

    Adding Bootstrap Library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import {FormsModule} from '@angular/forms'
   // used to register modules
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    NgbModule
 
   ],
   providers: [],
