14570dff572b96711d467442159b7cdb04a7bafc
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-09-01 09:17:55 +0530

    Material UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import {FormsModule} from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button'
+import {MatBadgeModule} from '@angular/material/badge'
 
 @NgModule({
 
@@ -19,7 +22,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
   imports: [
     BrowserModule,
     FormsModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatBadgeModule
 
   ],
   providers: [],
