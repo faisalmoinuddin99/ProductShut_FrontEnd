32cfee3ed498b335f08933333c63de976cd0604b
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-09-01 20:07:09 +0530

    Basic Typescript for angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'Template Reference Variable';
+  title: string = 'Basic of TypeScript for Angular';
 
-  getData(item: string){
-    console.log(item);
+ // how to define object and array type data types
+ data:{name: string, age: number, isActive: boolean} = {name: "jhon", age:23, isActive: true}
+
+ item:string [] = ["bread", "butter", "egg"]
+
+ loopOverArray(d:string []){
+  d.forEach(element => {
+    console.log(element);
     
-  }
+  });
+ }
+
+ functionCall(){
+  this.loopOverArray(this.item)
+ }
+
 }
